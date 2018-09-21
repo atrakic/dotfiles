1d653fa25d242bbf6d3838a6418afc005ee1fbcd
Admir Trakic <atr@ufst.dk>
2018-09-21 14:05:39 +0200

    Add jenkins slave port

--- bin/blueocean.sh
+++ bin/blueocean.sh
@@ -13,6 +13,7 @@ docker run -it -d \
     -u root \
     -e LOG_LEVEL=debug \
     -p 8080:8080 \
+    -p 5000:5000 \
     -v $(which docker):/bin/docker \
     -v "${DOCKER}"-data:/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
