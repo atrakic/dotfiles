7470bb719a3597e533ffd268b6e85e64244d7e6f
Admir Trakic <atr@ufst.dk>
2018-10-24 16:49:11 +0200

    Update

--- bin/kops-admin.sh
+++ bin/kops-admin.sh
@@ -2,87 +2,137 @@
 
 set -x
 
-AWS_DEFAULT_PROFILE=awskops
-
-export AWS_DEFAULT_PROFILE=awskops
-export AWS_DEFAULT_REGION=eu-west-1
-export KOPS_STATE_STORE=s3://devops23-1536755886 # states
-export CLUSTER_NAME=aaaadasdasdfasdfasdf
-export CLUSTER_DOMAIN=k8s.local
-
 BUCKET_NAME=$CLUSTER_NAME.$DOMAIN_NAME-$(date %s)
 SSH_KEY=$CLUSTER_DOMAIN.pub
+group_name=kops
+IP_ADDRESS=$(curl -s http://ipinfo.io/ip)
 
-aws=$(which aws) || exit 1
-kops=$(which kops) || exit 1
-kubectl=$(which kubectl) || exit 1
+export AWS_DEFAULT_PROFILE=${AWS_DEFAULT_PROFILE:-awskops}
+export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:-eu-west-1}
+export CLUSTER_NAME=${CLUSTER_NAME:-foo}
+export CLUSTER_DOMAIN=${CLUSTER_DOMAIN:-k8s.local}
+export KOPS_STATE_STORE=s3://$BUCKET_NAME
 
-$aws sts get-caller-identity || exit 1
-$kops version
 
-action="$1"
+print_versions() {
+  $kops version
+  $aws --version
+  $kubectl version
+}
 
-case "$action" in
+main() {
+  action="$1"
 
-  "createkeys")
-  aws ec2 create-key-pair --key-name "$SSH_KEY" | jq -r '.KeyMaterial' | tee $CLUSTER_NAME.pem
-  chmod 400 "$CLUSTER_NAME.pem"
-  ssh-keygen -y -f "$CLUSTER_NAME.pem" > "$CLUSTER_NAME.pub"
-  ;;
+  aws=$(which aws) || exit 1
+  kops=$(which kops) || exit 1
+  kubectl=$(which kubectl) || exit 1
 
-  "create")
-  MASTER_COUNT=3 #
-  NODE_COUNT=1
-  NODE_SIZE=t2.small
-  MASTER_SIZE=t2.small
-  ZONES=eu-west-1a,eu-west1b,eu-west-1c
-  NETWORKING=kubenet
-  KUBERNETES_VERSION=v1.8.4
-  AUTHORIZATION=RBAC
-  group_name=kops
-  $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name "$group_name"
-  $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name "$group_name"
-  $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name "$group_name"
-  $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IamFullAccess --group-name "$group_name"
-  $aws s3api create-bucket \
-    --bucket "$BUCKET_NAME" \
-    --create-bucket-configuration LocationConstraint="$AWS_DEFAULT_REGION"
-  export KOPS_STATE_STORE=s3://$BUCKET_NAME
-  echo "Creating $CLUSTER_NAME.$CLUSTER_DOMAIN"
-  $kops create cluster\
-    --name "$CLUSTER_NAME.$CLUSTER_DOMAIN"\
-    --master-count "$MASTER_COUNT"\
-    --node-count "$NODE_COUNT"\
-    --node-size "$NODE_SIZE"\
-    --master-size "$MASTER_SIZE"\
-    --zones "$ZONES"\
-    --master-zones "$ZONES"\
-    --ssh-public-key "$SH_PUBLIC_KEY"\
-    --networking "$NETWORKING"\
-    --kubernetes-version "$KUBERNETES_VERSION"\
-    --authorization "$AUTHORIZATION"\
-    --yes
-  ;;
+  $aws sts get-caller-identity || exit 1
 
-  "info")
-  echo "$CLUSTER_NAME.$CLUSTER_DOMAIN:"
-  $kops get cluster
-  $kubectl cluster-info
-  $kops validate cluster
-  ;;
+  case "$action" in
 
-  "delete")
-  $kops delete cluster \
-    --name "$CLUSTER_NAME.$CLUSTER_DOMAIN"\
-    --yes
-  sleep 1
-  $aws s3api delete-bucket --bucket "$BUCKET_NAME"
-  ;;
+    "createkeys")
+      $aws ec2 create-key-pair --key-name "$SSH_KEY" | jq -r '.KeyMaterial' | tee "$CLUSTER_NAME.pem"
+      chmod 400 "$CLUSTER_NAME.pem"
+      ssh-keygen -y -f "$CLUSTER_NAME.pem" > "$CLUSTER_NAME.pub"
+      cp -f "$CLUSTER_NAME.pem" ~/.ssh/
+      cp -f "$CLUSTER_NAME.pub" ~/.ssh/
+      ;;
 
-  *)
-  echo "<info>|<create>|<delete>"
-  ;;
+    "create_iam_group")
+      echo "*** Creating IAM group $group_name and attaching IAM policies: ***"
+      $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name "$group_name"
+      $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name "$group_name"
+      $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name "$group_name"
+      $aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IamFullAccess --group-name "$group_name"
+      ;;
 
-esac
+    "create_s3")
+      $aws s3api create-bucket \
+       --acl=private\
+       --bucket "$BUCKET_NAME" \
+       --create-bucket-configuration LocationConstraint="$AWS_DEFAULT_REGION" || exit 1
+      ;;
 
-echo "done"
+    "create_cluster")
+      $aws s3 ls "$BUCKET_NAME" &>/dev/null || exit 1
+      $aws iam get-group --group-name $group_name &>/dev/null || exit 1
+      ls -al "$CLUSTER_NAME.pem" || exit 1
+      ls -la "$CLUSTER_NAME.pub" || exit 1
+
+      MASTER_COUNT=3
+      NODE_COUNT=3
+      NETWORKING=kubenet
+      KUBERNETES_VERSION=v1.8.4
+      AUTHORIZATION=RBAC
+
+      echo "*** Creating $CLUSTER_NAME.$CLUSTER_DOMAIN: ***"
+
+      $kops create cluster\
+        --name "$CLUSTER_NAME.$CLUSTER_DOMAIN"\
+        --node-count "$NODE_COUNT"\
+        --master-count "$MASTER_COUNT"\
+        --node-size "$NODE_SIZE"\
+        --master-size "$MASTER_SIZE"\
+        --zones "$ZONES"\
+        --master-zones "$ZONES"\
+        --ssh-public-key "$SH_PUBLIC_KEY"\
+        --networking "$NETWORKING"\
+        --ssh-access="${IP_ADDRESS}/32"\
+        --admin-access="${IP_ADDRESS}/32" \
+        --kubernetes-version "$KUBERNETES_VERSION"\
+        --authorization "$AUTHORIZATION"\
+        --yes
+      #$kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.8.3.yaml
+    ;;
+
+  "create_private")
+      $aws s3 ls "$BUCKET_NAME" &>/dev/null || exit 1
+      $aws iam get-group --group-name $group_name &>/dev/null || exit 1
+
+      echo "*** Creating $CLUSTER_NAME.$CLUSTER_DOMAIN with private topology: ***"
+
+      # Create a cluster in AWS that has HA masters.  This cluster
+      # will be setup with an internal networking in a private VPC.
+      # A bastion instance will be setup to provide instance access.
+
+      $kops create cluster\
+          --name "$CLUSTER_NAME.$CLUSTER_DOMAIN"\
+          --node-count 3 \
+          --zones "$ZONES" \
+          --node-size "$NODE_SIZE" \
+          --master-size "$MASTER_SIZE" \
+          --master-zones "$ZONES" \
+          --networking weave \
+          --topology private \
+          --bastion="true" \
+          --yes
+      ;;
+
+    "status")
+      echo "Status: $CLUSTER_NAME.$CLUSTER_DOMAIN:"
+      $kops get cluster
+      $kubectl cluster-info
+      $kops validate cluster
+      ;;
+
+    "delete")
+      echo "*** Deleting $CLUSTER_NAME.$CLUSTER_DOMAIN: ***"
+      $kops delete cluster \
+        --name "$CLUSTER_NAME.$CLUSTER_DOMAIN"\
+        --state=s3://"${BUCKET_NAME}"\
+        --yes
+      sleep 1
+      echo "Deleting bucket: ${BUCKET_NAME}"
+      $aws s3 rm s3://"${BUCKET_NAME}" --recursive
+      $aws s3api delete-bucket --bucket "$BUCKET_NAME"
+      ;;
+
+    *)
+      echo "<info>|<create_cluster>|<create_group>|<delete>|<status>"
+      ;;
+
+  esac
+}
+
+main "$@"
