1743f409915860fff78655cde567ed85da7c3f58
Admir Trakic <xomodo@gmail.com>
2019-01-20 17:28:42 +0100

    Allways pull

--- bin/blueocean.sh
+++ bin/blueocean.sh
@@ -4,6 +4,8 @@ set -e
 
 DOCKER=blueocean
 
+docker pull jenkinsci/${DOCKER}
+
 docker volume create "${DOCKER}"-data
 
 docker run -it -d \
@@ -16,8 +18,9 @@ docker run -it -d \
     -p 5000:5000 \
     -v $(which docker):/bin/docker \
     -v "${DOCKER}"-data:/var/jenkins_home \
-    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$HOME":/home \
     jenkinsci/$DOCKER
+#    --privileged=true \
 #    -v "$(pwd)/seed":/var/jenkins_home/workspace/seed \
 # curl --user 'username:password' --data-urlencode "script=$(< ./somescript.groovy)" https://localhost:8080/scriptText
