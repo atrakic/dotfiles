31bb8c84d1d0598394d42324adebd723686747f1
Admir Trakic <xomodo@gmail.com>
2019-01-25 11:49:52 +0100

    Fix postgresql.sh

--- bin/postgresql.sh
+++ bin/postgresql.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-DOCKER=postgres
+DOCKER="postgres"
 POSTGRES_PASSWORD="test"
 POSTGRES_USER="test"
 
@@ -14,14 +14,13 @@ docker run -it -d \
     --label "$DOCKER" \
     --name "$DOCKER" \
     --rm \
-    -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD=}" \
+    -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
     -e POSTGRES_USER="${POSTGRES_USER}" \
     -p 5432:5432 \
     -v "${DOCKER}"-data:/var/lib/postgresql/data \
   ${DOCKER}
 
-
-echo PGHOST=0.0.0.0
-echo PGUSER=${POSTGRES_USER}
-echo PGPASSWORD=${POSTGRES_PASSWORD}
-echo PGDATABASE=postgres
+export PGHOST="0.0.0.0"
+export PGUSER="${POSTGRES_USER}"
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+export PGDATABASE="postgres"
--- bin/psql-helper.sh
+++ bin/psql-helper.sh
@@ -15,36 +15,38 @@ main() {
   do=$(basename "$thisfile" | cut -d. -f1)
 
   case "$do" in
-    psql_autovacuum_is_running)
-      psql -tAc "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query\
-        FROM pg_stat_activity\ 
-          WHERE upper(query) like '%VACUUM%'\
-          ORDER BY xact_start;"
+  psql_autovacuum_is_running)
+    psql -tAc "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query\
+      FROM pg_stat_activity\ 
+        WHERE upper(query) like '%VACUUM%'\
+        ORDER BY xact_start;"
     ;;
-    psql_current_database)
-      psql -tAc 'SELECT current_database();'
+  psql_current_database)
+    psql -tAc 'SELECT current_database();'
     ;;
-    psql_database_size)
-      psql -tAc "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname))\
-      FROM pg_database\
-      ORDER BY pg_database_size DESC;"
+  pg_databases)
+    psql -tAc "select * from pg_database;"
     ;;
-    psql_settings_not_default_or_override)
-      psql -tAc "SELECT name, current_setting(name), SOURCE
-          FROM pg_settings\
-          WHERE SOURCE NOT IN ('default', 'override');"
+  psql_database_size)
+    psql -tAc "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname))\
+    FROM pg_database\
+    ORDER BY pg_database_size DESC;"
     ;;
-    psql_settings)
-      # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Appendix.PostgreSQL.CommonDBATasks.html#Appendix.PostgreSQL.CommonDBATasks.PostGIS
-      psql -tAc 'SELECT name, setting, boot_val, reset_val, unit \
-        FROM pg_settings \
-        ORDER by name;'
+  psql_settings_not_default_or_override)
+    psql -tAc "SELECT name, current_setting(name), SOURCE
+        FROM pg_settings\
+        WHERE SOURCE NOT IN ('default', 'override');"
     ;;
-    psql_current_user)
-      psql -tAc 'SELECT current_user;'
+  psql_settings)
+    psql -tAc 'SELECT name, setting, boot_val, reset_val, unit \
+      FROM pg_settings \
+      ORDER by name;'
     ;;
-    psql_version)
-      psql -tAc 'SELECT version();'
+  psql_current_user)
+    psql -tAc 'SELECT current_user;'
+    ;;
+  psql_version)
+    psql -tAc 'SELECT version();'
     ;;
   esac
 }
