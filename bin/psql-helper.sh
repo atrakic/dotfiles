7996462b39452881996fe0a097967c3a2898b2ac
Admir Trakic <atr@ufst.dk>
2019-01-28 16:22:56 +0100

    Dont use backslash

--- bin/psql-helper.sh
+++ bin/psql-helper.sh
@@ -16,10 +16,7 @@ main() {
 
   case "$do" in
   psql_autovacuum_is_running)
-    psql -tAc "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query\
-      FROM pg_stat_activity\ 
-        WHERE upper(query) like '%VACUUM%'\
-        ORDER BY xact_start;"
+    psql -tAc "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query FROM pg_stat_activity WHERE upper(query) like '%VACUUM%' ORDER BY xact_start;"
     ;;
   psql_current_database)
     psql -tAc 'SELECT current_database();'
@@ -34,19 +31,13 @@ main() {
    psql -tAc "SELECT * FROM pg_stat_activity ORDER BY pid;"
    ;;
   psql_database_size)
-    psql -tAc "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname))\
-    FROM pg_database\
-    ORDER BY pg_database_size DESC;"
+    psql -tAc "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname)) FROM pg_database ORDER BY pg_database_size DESC;"
     ;;
   psql_settings_not_default_or_override)
-    psql -tAc "SELECT name, current_setting(name), SOURCE
-        FROM pg_settings\
-        WHERE SOURCE NOT IN ('default', 'override');"
+    psql -tAc "SELECT name, current_setting(name), SOURCE FROM pg_settings WHERE SOURCE NOT IN ('default', 'override');"
     ;;
   psql_settings)
-    psql -tAc 'SELECT name, setting, boot_val, reset_val, unit \
-      FROM pg_settings \
-      ORDER by name;'
+    psql -tAc 'SELECT name, setting, boot_val, reset_val, unit FROM pg_settings ORDER by name;'
     ;;
   psql_current_user)
     psql -tAc 'SELECT current_user;'
