77c81341eb0134c50c6b2148cbc62ca6e0e940ae
Admir Trakic <atr@ufst.dk>
2019-02-01 12:45:34 +0100

    List ext

--- bin/psql-helper.sh
+++ bin/psql-helper.sh
@@ -13,37 +13,44 @@ fi
 main() {
   thisfile="${0##*/}"
   do=$(basename "$thisfile" | cut -d. -f1)
+    arg="" # -t a c
 
   case "$do" in
   psql_autovacuum_is_running)
-    psql -tAc "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query FROM pg_stat_activity WHERE upper(query) like '%VACUUM%' ORDER BY xact_start;"
+    psql $arg "SELECT datname, usename, pid, state, wait_event, current_timestamp - xact_start AS xact_runtime, query FROM pg_stat_activity WHERE upper(query) like '%VACUUM%' ORDER BY xact_start;"
     ;;
   psql_current_database)
-    psql -tAc 'SELECT current_database();'
+    psql $arg 'SELECT current_database();'
     ;;
   pg_roles)
-    psql -tAc "SELECT rolname FROM pg_roles;"
+    psql $arg "SELECT rolname FROM pg_roles;"
     ;;
   pg_databases)
-    psql -tAc "select * from pg_database;"
+    psql $arg "select * from pg_database;"
     ;;
   pg_stat_activity)
-   psql -tAc "SELECT * FROM pg_stat_activity ORDER BY pid;"
+   psql $arg "SELECT * FROM pg_stat_activity ORDER BY pid;"
    ;;
   psql_database_size)
-    psql -tAc "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname)) FROM pg_database ORDER BY pg_database_size DESC;"
+    psql $arg "SELECT pg_database.datname, pg_database_size(pg_database.datname), pg_size_pretty(pg_database_size(pg_database.datname)) FROM pg_database ORDER BY pg_database_size DESC;"
     ;;
   psql_settings_not_default_or_override)
-    psql -tAc "SELECT name, current_setting(name), SOURCE FROM pg_settings WHERE SOURCE NOT IN ('default', 'override');"
+    psql $arg "SELECT name, current_setting(name), SOURCE FROM pg_settings WHERE SOURCE NOT IN ('default', 'override');"
     ;;
+  psql_extensions_available)  
+   psql -tAC "SELECT * FROM pg_available_extensions;"
+   ;;
+  psql_extensions_installed)
+   psql $arg "SELECT extname FROM pg_extension;"
+   ;;
   psql_settings)
-    psql -tAc 'SELECT name, setting, boot_val, reset_val, unit FROM pg_settings ORDER by name;'
+    psql $arg 'SELECT name, setting, boot_val, reset_val, unit FROM pg_settings ORDER by name;'
     ;;
   psql_current_user)
-    psql -tAc 'SELECT current_user;'
+    psql $arg 'SELECT current_user;'
     ;;
   psql_version)
-    psql -tAc 'SELECT version();'
+    psql $arg 'SELECT version();'
     ;;
   esac
 }
