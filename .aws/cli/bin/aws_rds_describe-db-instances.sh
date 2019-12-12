25b99ba36c56484ae120bc7f9e99f7a1a1317f90
Admir Trakic <atrakic@users.noreply.github.com>
2019-12-12 22:02:20 +0100

    Update aws_rds_describe-db-instances.sh

--- .aws/cli/bin/aws_rds_describe-db-instances.sh
+++ .aws/cli/bin/aws_rds_describe-db-instances.sh
@@ -1,6 +1,6 @@
 
 # db instances
-aws rds describe-db-instances --output table --query 'DBInstances[].[Engine,DBInstanceIdentifier,DBInstanceIdentifier,DBInstanceClass,LatestRestorableTime]'
+aws rds describe-db-instances --output table --query 'DBInstances[].[Engine,DBInstanceIdentifier,DBInstanceIdentifier,DBInstanceClass,BackupRetentionPeriod,LatestRestorableTime]'
 
 # cluster snapshots:
 for id in `aws rds describe-db-clusters --query 'DBClusters[].DBClusterIdentifier' --output text`; do 
